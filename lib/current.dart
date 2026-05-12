5dddcb9a820835db3449224a6b45578c996bc7f2
jshep23 <justin.shepertycki@gmail.com>
2026-05-12 17:01:32 -0500

    add ref to current_core. export current_core as part of current (backwards compat)

--- lib/current.dart
+++ lib/current.dart
@@ -2,9 +2,8 @@
 library;
 
 export 'src/current.dart';
-export 'src/current_property.dart';
 export 'src/current_view_model.dart';
 export 'src/current_widget.dart';
-export 'src/current_cloneable.dart';
 export 'src/current_text_controller.dart';
 export 'src/current_validation.dart';
+export 'package:current_core/current_core.dart';
